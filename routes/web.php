37e074f6fee4ad9b840a648e53bc364dad596ce8
LalitBhusal777 <labh_bca2077@lict.edu.np>
2025-02-28 09:52:52 +0545

    make the authenticate

--- routes/web.php
+++ routes/web.php
@@ -6,36 +6,38 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('/book/{id}',[HomeController::class,'detail'])->name('book.detail');
-Route::post('/save-book-review',[HomeController::class,'saveReview'])->name('book.saveReview');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/book/{id}', [HomeController::class, 'detail'])->name('book.detail');
+Route::post('/save-book-review', [HomeController::class, 'saveReview'])->name('book.saveReview');
 
-Route::group(['prefix'=>'account'],function(){
-    Route::group(['middleware'=>'guest'],function(){
-        Route::get('register',[AccountController::class,'register'])->name('account.register');
-        Route::get('login',[AccountController::class,'login'])->name('account.login');
-        Route::post('processRegister',[AccountController::class,'processRegister'])->name('account.processRegister');
-        Route::post('login',[AccountController::class,'authinticate'])->name('account.authinticate');
-
+Route::group(['prefix' => 'account'], function () {
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('register', [AccountController::class, 'register'])->name('account.register');
+        Route::get('login', [AccountController::class, 'login'])->name('account.login');
+        Route::post('processRegister', [AccountController::class, 'processRegister'])->name('account.processRegister');
+        Route::post('login', [AccountController::class, 'authinticate'])->name('account.authinticate');
     });
-    Route::group(['middleware'=>'auth'],function(){
-        Route::get('profile',[AccountController::class,'profile'])->name('account.profile');
-        Route::get('logout',[AccountController::class,'logout'])->name('account.logout');
-        Route::post('update-profile',[AccountController::class,'updateProfile'])->name('account.updateProfile');
-        Route::get('books',[BookController::class,'index'])->name('books.index');
-        Route::get('books/create',[BookController::class,'create'])->name('books.create');
-        Route::post('books/store',[BookController::class,'store'])->name('books.store');
-        Route::get('books/edit/{id}',[BookController::class,'edit'])->name('books.edit');
-        Route::put('books/edit/{id}',[BookController::class,'update'])->name('books.update');
-        Route::delete('books/{book}',[BookController::class,'destroy'])->name('books.destroy');
-        Route::get('reviews',[ReviewController::class,'index'])->name('account.reviews');
-        Route::get('reviews/{id}',[ReviewController::class,'edit'])->name('account.reviews.edit');
-        Route::post('reviews/{id}',[ReviewController::class,'updateReview'])->name('account.reviews.updateReview');
-        Route::post('delete-review',[ReviewController::class,'deleteReview'])->name('account.reviews.deleteReview');
-        Route::get('my-reviews',[AccountController::class,'myReviews'])->name('account.myReviews');
-        Route::get('my-reviews/{id}',[AccountController::class,'editReview'])->name('account.myReviews.editReview');
-        Route::post('my-reviews/{id}',[AccountController::class,'updateReview'])->name('account.myReviews.updateReview');
-        Route::delete('delete-my-review',[AccountController::class,'deleteReview'])->name('account.myReviews.deleteReview');
-    });
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('profile', [AccountController::class, 'profile'])->name('account.profile');
+        Route::get('logout', [AccountController::class, 'logout'])->name('account.logout');
+        Route::post('update-profile', [AccountController::class, 'updateProfile'])->name('account.updateProfile');
 
+        Route::group(['middleware' => 'check-addmin'], function () {
+            Route::get('books', [BookController::class, 'index'])->name('books.index');
+            Route::get('books/create', [BookController::class, 'create'])->name('books.create');
+            Route::post('books/store', [BookController::class, 'store'])->name('books.store');
+            Route::get('books/edit/{id}', [BookController::class, 'edit'])->name('books.edit');
+            Route::put('books/edit/{id}', [BookController::class, 'update'])->name('books.update');
+            Route::delete('books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+            Route::get('reviews', [ReviewController::class, 'index'])->name('account.reviews');
+            Route::get('reviews/{id}', [ReviewController::class, 'edit'])->name('account.reviews.edit');
+            Route::post('reviews/{id}', [ReviewController::class, 'updateReview'])->name('account.reviews.updateReview');
+            Route::post('delete-review', [ReviewController::class, 'deleteReview'])->name('account.reviews.deleteReview');
+        });
+        
+        Route::get('my-reviews', [AccountController::class, 'myReviews'])->name('account.myReviews');
+        Route::get('my-reviews/{id}', [AccountController::class, 'editReview'])->name('account.myReviews.editReview');
+        Route::post('my-reviews/{id}', [AccountController::class, 'updateReview'])->name('account.myReviews.updateReview');
+        Route::delete('delete-my-review', [AccountController::class, 'deleteReview'])->name('account.myReviews.deleteReview');
+    });
 });
